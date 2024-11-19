2c996a398419412786fcbedb2b127c9afab3a3d4
Grigorii Sharapov <socaseinpoint@gmail.com>
2024-11-19 17:13:02 -0300

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <p className="text-4xl font-bold">REACT + Tailwind Boilerplate</p>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="flex h-screen items-center justify-center">
+        <p className="text-4xl font-bold">REACT + Tailwind Boilerplate</p>
+      </div>
+    </QueryClientProvider>
   )
 }
 
