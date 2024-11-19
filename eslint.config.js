1e602d51051d224b46c19d453c406e9e85459fbc
Grigorii Sharapov <socaseinpoint@gmail.com>
2024-11-19 14:56:32 -0300

    feat: add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ import tseslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
 
 export default [
   {
@@ -17,11 +20,42 @@ export default [
     plugins: {
       '@typescript-eslint': tseslint,
       prettier,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
       ...prettierConfig.rules,
+      /* -- a11y rules -- */
+      ...jsxA11yPlugin.configs.recommended.rules,
+      'jsx-a11y/control-has-associated-label': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      /* -- react rules -- */
+      ...reactPlugin.configs.recommended.rules,
+      'eslintreact/function-component-definition': 'off',
+      'react/require-default-props': 'off',
+      'react/function-component-definition': 'off',
+      'react/no-array-index-key': 'off',
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+      'react/jsx-uses-react': 'off', // Отключает необходимость React в области видимости
+      'react/react-in-jsx-scope': 'off', // Отключает необходимость импортировать React
+      'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+      'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies,
+      /* -- prettier -- */
       'prettier/prettier': 'error',
     },
   },
