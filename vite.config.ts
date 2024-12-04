e87cca50473f82e06a99633f6acdb0988bdaf2ad
Grigorii Sharapov <socaseinpoint@gmail.com>
2024-12-04 19:48:56 -0300

    feat: add autoprefixer tailwindcss viteImagemin in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,70 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
+
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name === 'index.css') {
+            return 'assets/[name]-[hash].css'
+          }
+          if (assetInfo.name && /\.(svg|png)$/.test(assetInfo.name)) {
+            return 'assets/[name]-[hash][extname]'
+          }
+          return assetInfo.name
+            ? 'assets/[name][extname]'
+            : 'assets/[hash][extname]'
+        },
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 75, // Уровень сжатия JPEG (0-100)
+      },
+      pngquant: {
+        quality: [0.65, 0.8], // Диапазон качества для PNG
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
